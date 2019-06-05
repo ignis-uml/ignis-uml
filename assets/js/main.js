c944e6caba5ef5e44a1c6b41d763bcf9aae215d7
ignis-uml <ignis.uml@gmail.com>
2019-06-05 22:20:57 +0200

    Adds webpack, vue

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+var app = new Vue({
+    delimiters: ['${', '}'],
+    el: '#x',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
