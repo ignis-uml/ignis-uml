68012d420d18c10ff29efa91ff42c846aa78d402
ignis-uml <ignis.uml@gmail.com>
2022-04-07 12:52:51 +0200

    Add rendering

--- frontend/src/js/view.js
+++ frontend/src/js/view.js
@@ -26,20 +26,33 @@ View.render = function(objects) {
 View.packObjects = function(objects) {
     var packedObjects = {};
 
-    packedObjects['rows'] = [];
+    // packedObjects['rows'] = [];
+    packedObjects= [];
 
 
     $.each(objects, function(index, object) {
         textLength = object.label.length;
         console.log(textLength);
 
-        position = {}
-        config = {}
+        position = {
+            left: 0,
+            right: 15,
+            top: 0,
+            bottom: 5,
+        }
+        config = {
+            size: {
+                width: 100,
+                height: 50,
+            }
+        }
 
-        packedObjects['level-1'][] = ({
+        packedObjects[packedObjects.length++] = ({
             object: object,
             position: position,
             config: config,
         });
     });
+
+    return packedObjects;
 }
